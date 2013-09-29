1fbbf3bdb99f085e3de8760f741ae693a041c816
Enrique Albert <enrique.albert.gleiser@gmail.com>
2013-09-29 17:58:17 +0100

    Routine Review - Pump
    Almost there

--- pages/encounter/routineVisit.php
+++ pages/encounter/routineVisit.php
@@ -5,13 +5,12 @@ $header_stylesheet = '<link href="../../css/main.css" rel="stylesheet" type="tex
 $header_stylesheet .= '<link href="../../css/review.css" rel="stylesheet" />';
 $header_stylesheet .= '<link href="../../css/jquery.handsontable.full.css" rel="stylesheet" />';
 $header_scripts = '<script src="../../scripts/jquery.handsontable.full.js"></script>';
-$header_scripts .= '<script src="../../jquery.maskedinput.js" type="text/javascript"></script>';
 $today = date("Y-m-d");
 require_once('/../common/header.php');
 ?>
 <article id="PatientSummary" class="body">
     <header>
-        <h2>New Routine Visit</h2>
+        <h2>New Routine Visit [PUMP]</h2>
     </header>
     <?php require('/../common/patientBoard.php');?>
 </article>
@@ -92,12 +91,135 @@ require_once('/../common/header.php');
                 <input type="button" value="Previous" onclick="showForm('formTwo', 'formOne')" />
             </fieldset>
             <fieldset class="formThree" style="display: none;">
-                <a href="routineVisitPdf.php" target="_blank" style="float: right;" >Print Form</a>
-                <h2>Comments</h2>
-                <textarea rows="10" cols="100"></textarea>
-                <input type="submit" value="Save" style="float: right;"/>
+                <div>
+                    <div style="display: inline-table; width: 40%;">
+                        <h2>Insulin - Carb Ratio</h2>
+                        <div id="carbRatio" class="handsontable"></div>
+                        <br/>
+                    </div>
+                    <div style="display: inline-table; width: 40%;">
+                        <h2>Insulin Sensitivity Factor</h2>
+                        <div id="isf" class="handsontable"></div>
+                    </div>
+                </div>
+                <input type="button" value="Next" onclick="showForm('formThree', 'formFour')" style="float: right;" />
                 <input type="button" value="Previous" onclick="showForm('formThree', 'formTwo')" />
-
+            </fieldset>
+            <fieldset class="formFour" style="display: none;">
+                <h2>Nursing Notes</h2>
+                <div style="display: inline-table; width: 50%">
+                    <label class="field widerColumn">Sick Day Rules:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">Ketone Testing:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">Record Keeping:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">Alcohol:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">Contraception:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                </div>
+                <div style="display: inline-table;">
+                    <label class="field widerColumn">Hypoglycaemia rules:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">Dietary Issues:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">School:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                    <label class="field widerColumn">Smoking:</label>
+                    <select>
+                        <option value="true">Yes</option>
+                        <option value="false" selected>No</option>
+                    </select>
+                    <br/>
+                </div>
+                <br/><br/>
+                <h3>Comments</h3>
+                <textarea rows="6" cols="50"></textarea>
+                <input type="button" value="Next" onclick="showForm('formFour', 'formFive')" style="float: right;" />
+                <input type="button" value="Previous" onclick="showForm('formFour', 'formThree')" />
+            </fieldset>
+            <fieldset class="formFive" style="display: none;">
+                <div>
+                    <div style="display: inline-table; width: 50%;">
+                        <h2>Insulin - Carb Ratio</h2>
+                        <div id="dietHistory" class="handsontable"></div>
+                        <br/>
+                    </div>
+                    <div id="foodGroups" style="display: inline-table; width: 25%;">
+                        <h2>Food Groups Checklist</h2>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Energy</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Iron</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Protein</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Oily Fish</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Fruit & Veg</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Legumes</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Calcium</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Multivit/Mins</label><br/>
+                    </div>
+                    <div id="carbCounting" style="display: inline-table; width: 20%;">
+                        <h2>Carb Counting</h2>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Basic (Discussed at Dx)</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Reading Labels</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Weighing Portions</label><br/>
+                        <input type="checkbox">
+                        <label class="field widerColumn">Competent</label><br/>
+                    </div>
+                </div>
+                <input type="button" value="Next" onclick="showForm('formFive', 'formSix')" style="float: right;" />
+                <input type="button" value="Previous" onclick="showForm('formFive', 'formFour')" />
+            </fieldset>
+            <fieldset class="formSix" style="display: none;">
+                <a href="routineVisitPdf.php" target="_blank" style="float: right;" >Print Form</a>
+                <h2>Medical Notes</h2>
+                <textarea rows="6" cols="50"></textarea>
+                <h2>Extra Notes</h2>
+                <textarea rows="6" cols="50"></textarea>
+                <input type="submit" value="Save" style="float: right;"/>
+                <input type="button" value="Previous" onclick="showForm('formSix', 'formFive')" />
             </fieldset>
         </form>
     </section>
@@ -117,6 +239,21 @@ require_once('/../common/header.php');
 
         var activeIns = [
             [0, 0]
+        ];
+
+        var carbRatios = [
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0]
+        ];
+
+        var isfRecords = [
+            [0, 0, 0]
+        ];
+
+        var dietHistories = [
+            [""], [""], [""], [""], [""], [""], [""]
         ];
 
         $('#basalRates').handsontable({
@@ -158,11 +295,53 @@ require_once('/../common/header.php');
             ]
         });
 
+        $('#carbRatio').handsontable({
+            data: carbRatios,
+            minSpareRows: 0,
+            colHeaders: ["Old", "New"],
+            contextMenu: false,
+            colWidths: 100,
+            columns: [
+                {type: 'numeric', format: '0,0'},
+                {type: 'numeric', format: '0,0'}
+            ],
+            rowHeaders: ["Breakfast", "Lunch", "Dinner", "Bedtime"]
+        });
+
+
+        $('#isf').handsontable({
+            data: isfRecords,
+            minSpareRows: 1,
+            maxRows: 4,
+            colHeaders: ["Time", "Old", "New"],
+            contextMenu: false,
+            colWidths: 100,
+            columns: [
+                {type: 'text'},
+                {type: 'numeric', format: '0,0'},
+                {type: 'numeric', format: '0,0'}
+            ]
+        });
+
+        $('#dietHistory').handsontable({
+            data: dietHistories,
+            minSpareRows: 0,
+            rowHeaders: false,
+            contextMenu: false,
+            colWidths: 400,
+            columns: [
+                {type: 'text'}
+            ],
+            rowHeaders: ["Breakfast", "Snack", "Lunch", "Snack", "Diner", "Bedtime", "Sports"]
+        });
+
+
+
     });
     function showForm(formToClose, formToShow){
         $("." + formToClose).hide();
         $("." + formToShow).show();
-    }
+    };
 
 </script>
 <?php
