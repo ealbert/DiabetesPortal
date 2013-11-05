566ab409d830768c7f10fa89d909963adb8a3538
Enrique Albert <enrique.albert.gleiser@gmail.com>
2013-11-05 13:49:49 +0000

    QL#31 Telephone Encounter
    the insulin doses with the 24 hour clock- what the nurses record is
    blood glucose +/-  blood ketones (if high) and then insulin delivered in
    terms of meal bolus +/- correction bolus- ie up to 4 items at each time
    point. They generally record 4 days at a time. So they would want 8 (or
    16…) columns reflecting 4 days readings.

--- pages/encounter/doc_pump_doses.php
+++ pages/encounter/doc_pump_doses.php
@@ -1,6 +1,7 @@
 <div>
     <div style="display: inline-table; width: 90%;">
         <h2>Pump - Doses</h2>
+        <div id="dates" class="handsontable"></div>
         <div id="pumpDoses" class="handsontable"></div>
         <br/>
     </div>
@@ -9,18 +10,49 @@
     $(document).ready(function(){
 
         var pumpDoses = [
-            ["", "", "", "", ""]
+            ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
         ];
 
         $('#pumpDoses').handsontable({
             data: pumpDoses,
             minSpareRows: 0,
             contextMenu: false,
-            minRows: 26,
-            colWidths: 100,
-            rowHeaders: ["Date", "Day", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"
-                , "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+            minRows: 24,
+            colWidths: 50,
+            colHeaders: ["BC", "BK", "INS", "MB", "BC", "BK", "INS", "MB", "BC", "BK", "INS", "MB", "BC", "BK", "INS", "MB"],
+            rowHeaders: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"
+                , "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+            afterChange: function(changes, source){
+                $("#pumpDoses table tr  td:nth-child(5)").css("border-right", "2px solid black");
+                $("#pumpDoses table tr  td:nth-child(9)").css("border-right", "2px solid black");
+                $("#pumpDoses table tr  td:nth-child(13)").css("border-right", "2px solid black");
+                $("#pumpDoses table tr  td").css("text-align", "center");
+            }
         });
+
+        var dates = [
+            ["", "", "", ""]
+        ];
+
+        $('#dates').handsontable({
+            data: dates,
+            minSpareRows: 0,
+            contextMenu: false,
+            minRows: 2,
+            colWidths: 200,
+            rowHeaders: ["Date", "Day"],
+            afterChange: function(changes, source){
+                $("#dates table tr  td:nth-child(2)").css("border-right", "2px solid black");
+                $("#dates table tr  td:nth-child(3)").css("border-right", "2px solid black");
+                $("#dates table tr  td:nth-child(4)").css("border-right", "2px solid black");
+                $("#dates table tr  td").css("text-align", "center");
+            }
+        });
+
+
+
+
+
 
     });
 
